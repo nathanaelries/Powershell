e4c70bade5ba5e9b91ed2902428a067480463393
nathanael.ries <nathanael.ries@gmail.com>
2020-11-06 16:10:59 -0600

    Create Import-OverlayCSVtoSQL.ps1

---
+++ Import-OverlayCSVtoSQL.ps1
@@ -0,0 +1,70 @@
+<#
+# This is a work in progress. The intention is to load a csv and overlay the values into an existing sql database, overwriting the sql values within.
+#
+>#
+
+$ServerInstance = "LEGUS-SQL-MI01\INSTANCEA"
+$Database = "prj_SCOx0001_SGSS_MINDSEYE_1382"
+$LoadfilePath = "C:\Users\nd-admin\Desktop\test.csv"
+
+$ImportedLoadfile = Import-Csv -Path $LoadfilePath -Delimiter "`t"
+
+# get the loadfile headers and store them as fields
+$LoadfileFields = $ImportedLoadfile | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+
+$SQLFieldPlaceholder = @()
+
+$OFS = "`r`n"
+
+$SQLSelect = @"
+SELECT  
+"@
+
+$SQLFrom = @"
+FROM 
+"@
+
+$SQLJoins = @"
+WHERE 
+"@
+
+$SQLWhere = @"
+AND 
+"@
+
+$X = 1
+$SQLFieldPlaceholder = @()
+$SQLValuePlaceholder = @()
+$SQLTablePlaceholder = @()
+$SQLJoinsPlaceholder = @()
+
+function Generate-SelectStatement($Field,$Int){
+return ("F"+'{0:d3}' -f $Int+".$field as F"+'{0:d3}' -f $Int+"$field")
+}
+# Generate a select statement
+foreach($LFfield in $LoadfileFields){
+# Generate a select statement
+$SQLFieldPlaceholder +=  Generate-SelectStatement -Field TABLE_CATALOG -Int $X
+$SQLFieldPlaceholder +=  Generate-SelectStatement -Field TABLE_NAME -Int $X
+$SQLFieldPlaceholder +=  Generate-SelectStatement -Field COLUMN_NAME -Int $X
+
+#Generate FROM clause
+$SQLTablePlaceholder += ("INFORMATION_SCHEMA.COLUMNS F"+'{0:d3}' -f $X)
+
+# Generate where
+$SQLJoinsPlaceholder +=  ("F001.TABLE_NAME = F"+'{0:d3}' -f $X+".TABLE_NAME ")
+
+# Generate and
+$SQLValuePlaceholder +=  ("F"+'{0:d3}' -f $X+++".COLUMN_NAME = '$LFField' ")
+}
+
+$SQLSelect = $SQLSelect + ($SQLFieldPlaceholder -join ", $OFS")
+$SQLFrom = $SQLFrom + ($SQLTablePlaceholder -join ", $OFS")
+$SQLJoins = $SQLJoins + (($SQLJoinsPlaceholder | Select -Skip 1) -join "AND $OFS")
+$SQLWhere = $SQLWhere + ($SQLValuePlaceholder -join "AND $OFS")
+
+$QUERY = $SQLSelect + $OFS + $SQLFrom + $OFS + $SQLJoins + $OFS + $SQLWhere
+
+$PotentialTables = Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $Database -Query $QUERY -QueryTimeout 65524
+
+$PotentialTables.
