adb34cd09ec04d1984f2d9d1a2c9298f3e645848
nathanael.ries <nathanael.ries@gmail.com>
2020-12-29 09:06:14 -0600

    Create Logoff-UserAcrossDomain.ps1

---
+++ Logoff-UserAcrossDomain.ps1
@@ -0,0 +1,82 @@
+function Logoff-UserAcrossDomain{
+     <#
+    .SYNOPSIS
+        Logoff-UserAcrossDomain is a powershell function to logout a specified user from all computers accros a domain.
+    .DESCRIPTION
+        Logoff-UserAcrossDomain is a powershell function to logout a specified user from all computers accros a domain.
+        It has one required parameter (switch): -ADLogonName
+        And one optional parameter, -Credential. Find the details of the parameters below.
+    .PARAMETER ADLogonName
+        Specifies the AD user logon name of the AD user to logout.
+    .Parameter Credential
+        Specify the credenial to run the AD commands as a different user.
+    .EXAMPLE
+        PS C:\> Logoff-UserAcrossDomain -ADLogonName ALICE -Credential (Get-Credential)
+    #>
+
+    [CmdletBinding()]
+    param(
+    [Parameter(Position = 0,Mandatory=$true)][string]$ADLogonName,
+    [ValidateNotNull()]
+    [System.Management.Automation.PSCredential]
+    [System.Management.Automation.Credential()]
+    $Credential = [System.Management.Automation.PSCredential]::Empty 
+    )
+    # List all Domain Controllers, Servers, and non-server computers
+    if($Credential -ne [System.Management.Automation.PSCredential]::Empty) {
+    [System.Collections.ArrayList]$Computers =  Get-ADComputer -Filter 'enabled -eq "true"' -Credential $Credential `
+    -Properties Name,Operatingsystem,OperatingSystemVersion |
+    Sort-Object -Property OperatingsystemVersion |
+    Select-Object -ExpandProperty Name
+    }else{
+    [System.Collections.ArrayList]$Computers =  Get-ADComputer -Filter 'enabled -eq "true"' `
+    -Properties Name,Operatingsystem,OperatingSystemVersion |
+    Sort-Object -Property OperatingsystemVersion |
+    Select-Object -ExpandProperty Name
+    }
+
+    # Remove current computer from list
+    $Computers.RemoveAt($Computers.IndexOf($Computers.where({$_ -eq $env:COMPUTERNAME})))
+
+    # Create Error Log Array
+    $ErrorLog = @()
+
+     ## Run through all the computers in the array
+     foreach($Computer in $Computers){
+     $computer
+
+        ## Find all sessions matching the specified username
+        $Procs = $null
+        $SessionIDs = @()
+        $Object = $null
+        try{
+        if($Credential -ne [System.Management.Automation.PSCredential]::Empty) {
+        $Procs = Invoke-Command $Computer -ErrorAction Stop -Credential $Credential -ScriptBlock{Get-Process -IncludeUserName -Name taskhost | Where-Object {$_.username -match "$ADLogonName"}} 
+        } else {
+        $Procs = Invoke-Command $Computer -ErrorAction Stop -ScriptBlock{Get-Process -IncludeUserName -Name taskhost | Where-Object {$_.username -match "$ADLogonName"}} 
+        }
+        If ($Procs) {
+            Foreach ($P in $Procs) {
+                $Object = New-Object PSObject -Property ([ordered]@{    
+                            "SessionID"              = $P.SessionID
+                })
+            $SessionIDs += $Object
+            }
+        }
+        Write-Host "Found $(@($sessionIds.SessionID).Count) user login(s) on computer."
+        ## Loop through each session ID and pass each to the logoff command
+        $sessionIds | ForEach-Object {
+            Write-Host "Logging off session id [$($_.SessionID)]..."
+            if($Credential -ne [System.Management.Automation.PSCredential]::Empty) {
+            Invoke-Command $Computer -ErrorAction Stop -Credential $Credential -ScriptBlock{logoff.exe $_.SessionID}
+            } else {
+            Invoke-Command $Computer -ErrorAction Stop -ScriptBlock{logoff.exe $_.SessionID}
+            }
+        }
+        } catch {  
+           
+        }
+
+    }
+
+}
